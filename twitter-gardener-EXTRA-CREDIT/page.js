6b36859dff95364bc57bb2b8440bb15c2d5c1f05
KB <popotwtw@gmail.com>
2019-05-03 01:31:13 +0800

    Add files via upload

--- twitter-gardener-EXTRA-CREDIT/page.js
+++ twitter-gardener-EXTRA-CREDIT/page.js
@@ -12,11 +12,13 @@ const POSITIVE_MESSAGES = [
 const curserUrl=chrome.runtime.getURL('images/rose-cursor.gif');
 const backUrl=chrome.runtime.getURL('images/sparkle.gif');
 
-chrome.runtime.onConnect.addListener(function(port) {
+chrome.runtime.onConnect.addListener(function(port)
+{
   port.onMessage.addListener(onMessage);
 });
 
-function onMessage(gardeningInProgress) {
+function onMessage(gardeningInProgress)
+{
 	// TODO(you): Implement this function for extra credit! Add helper functions
 	// as needed.
 
@@ -30,41 +32,45 @@ function onMessage(gardeningInProgress) {
 		tweets = document.getElementsByClassName('tweet')
 		for(let i=0,tweets_length=tweets.length;i<tweets_length;i++)
 		{
-			if(tweets[i].getAttribute('hacked')!='true')
+			if(tweets[i].getAttribute('hacked') != 'true')
 			{
-				tweets[i].addEventListener('click',function(e){gardening(e,tweets[i])});
-				tweets[i].addEventListener('mouseover',function(){over(tweets[i])});
-				tweets[i].addEventListener('mouseout',function(){out(tweets[i])});
+				tweets[i].addEventListener('click',function(seed){planting(seed,tweets[i])});
+				tweets[i].addEventListener('mouseover',function(){cover(tweets[i])});
+				tweets[i].addEventListener('mouseout',function(){present(tweets[i])});
 				tweets[i].setAttribute('hacked','true');
 				tweets[i].setAttribute('inprogress','true');
 			}
 		}
 
-		var code = "const POSITIVE_MESSAGES = ['Never gonna give you up','Never gonna let you down','Never gonna run around','Never gonna desert you','Never gonna make you cry','Never gonna say goodbye','Never gonna tell a lie','Never gonna hurt you'];\n"+
-		"function over(tweet){if(tweet.getAttribute('inprogress')=='false')return;const curserUrl="+"'"+curserUrl+"'"+";const backUrl="+"'"+backUrl+"'"+";tweet.style.cursor='url('+curserUrl+')4 12,auto';tweet.style.backgroundImage='url('+backUrl+')';tweet.style.opacity='0.8';}\n"+
-		"function out(tweet){if(tweet.getAttribute('inprogress')=='false')return;tweet.style.cursor='';tweet.style.backgroundImage='';tweet.style.opacity='1.0';}\n"+
-		"function gardening(e,tweet){if(tweet.getAttribute('inprogress')=='false')return;e.stopPropagation();if(tweet.getAttribute('gardened')=='true')return;let children=tweet.children;for(let i=0,cl=children.length;i<cl;i++)if(children[i].className=='content'){let x=children[i].children;for(let j=0,ccl=x.length;j<ccl;j++){if(x[j].className=='js-tweet-text-container')x[j].innerText=POSITIVE_MESSAGES[Math.floor(Math.random()*POSITIVE_MESSAGES.length)];if(x[j].className.includes('QuoteTweet')){y=x[j].children[0];y.children[0].setAttribute('href','https://www.youtube.com/watch?v=dQw4w9WgXcQ');y.children[1].setAttribute('href','https://www.youtube.com/watch?v=dQw4w9WgXcQ');}}}tweet.setAttribute('gardened','true');}\n"+
-		'('+function() {
+		var message = "const POSITIVE_MESSAGES = ['Never gonna give you up','Never gonna let you down','Never gonna run around','Never gonna desert you','Never gonna make you cry','Never gonna say goodbye','Never gonna tell a lie','Never gonna hurt you'];\n"+
+		"function cover(tweet){if(tweet.getAttribute('inprogress')=='false')return;const curserUrl="+"'"+curserUrl+"'"+";const backUrl="+"'"+backUrl+"'"+";tweet.style.cursor='url('+curserUrl+')4 12,auto';tweet.style.backgroundImage='url('+backUrl+')';tweet.style.opacity='0.8';}\n"+
+		"function present(tweet){if(tweet.getAttribute('inprogress')=='false')return;tweet.style.cursor='';tweet.style.backgroundImage='';tweet.style.opacity='1.0';}\n"+
+		"function planting(seed,tweet){if(tweet.getAttribute('inprogress')=='false')return;seed.stopPropagation();if(tweet.getAttribute('gardened')=='true')return;let children=tweet.children;for(let i=0,children_len=children.length;i<children_len;i++)if(children[i].className=='content'){let x=children[i].children;for(let j=0,x_len=x.length;j<x_len;j++){if(x[j].className=='js-tweet-text-container')x[j].innerText=POSITIVE_MESSAGES[Math.floor(Math.random()*POSITIVE_MESSAGES.length)];if(x[j].className.includes('QuoteTweet')){y=x[j].children[0];y.children[0].setAttribute('href','https://www.youtube.com/watch?v=dQw4w9WgXcQ');y.children[1].setAttribute('href','https://www.youtube.com/watch?v=dQw4w9WgXcQ');}}}tweet.setAttribute('gardened','true');}\n"+
+		'(' + function() {
 			$(document).ajaxComplete(function(){ 
 				tweets=document.getElementsByClassName('tweet');
+				
 				for(let i=0,tweets_length=tweets.length;i<tweets_length;i++)
 				{
 					if(tweets[i].getAttribute('hacked')!='true')
 					{
-						tweets[i].addEventListener('click',function(e){gardening(e,tweets[i])});
-						tweets[i].addEventListener('mouseover',function(){over(tweets[i])});
-						tweets[i].addEventListener('mouseout',function(){out(tweets[i])});
+						tweets[i].addEventListener('click',function(seed){planting(seed,tweets[i])});
+						tweets[i].addEventListener('mouseover',function(){cover(tweets[i])});
+						tweets[i].addEventListener('mouseout',function(){present(tweets[i])});
 						tweets[i].setAttribute('hacked','true');
 						tweets[i].setAttribute('inprogress','true');
 					}
 				}
 			});
 		}+')();';
-		if(document.getElementById('ajaxLoadDetect') == null)
+		
+		signal = document.getElementById('ajaxLoadDetect')
+		
+		if(signal == null)
 		{
 			var script = document.createElement('script');
 			script.setAttribute('id','ajaxLoadDetect')
-			script.textContent = code;
+			script.textContent = message;
 			document.head.appendChild(script);
 			script.parentNode.removeChild(script);
 		}
@@ -72,40 +78,53 @@ function onMessage(gardeningInProgress) {
 	
 	else
 	{
-		elem = document.getElementById('ajaxLoadDetect');
-		if(elem != null)
-			elem.parentNode.removeChild(elem);
+		signal = document.getElementById('ajaxLoadDetect');
+		
+		if(signal != null)
+		{
+			signal.parentNode.removeChild(signal);
+		}
+		
 		tweets=document.getElementsByClassName('tweet');
+		
 		for(let i=0,tweets_length=tweets.length;i<tweets_length;i++)
+		{
 				tweets[i].setAttribute('inprogress','false');
+		}
 	}
 }
 
-function over(tweet)
-{
-	if(tweet.getAttribute('inprogress') == 'false')
-		return;
-	tweet.style.cursor='url('+curserUrl+')4 12,auto';
-	tweet.style.backgroundImage='url('+backUrl+')';
-	tweet.style.opacity='0.8';
-}
-
-function out(tweet)
-{
-	if(tweet.getAttribute('inprogress') == 'false')
-		return;
-	tweet.style.cursor='';
-	tweet.style.backgroundImage='';
-	tweet.style.opacity='1.0';
-}
-
-function gardening(e,tweet)
+function cover(tweet)
 {
 	if(tweet.getAttribute('inprogress') == 'false')
 	{
 		return;
 	}
-	e.stopPropagation();
+	
+	tweet.style.cursor='url('+curserUrl+')4 12,auto';
+	tweet.style.backgroundImage='url('+backUrl+')';
+	tweet.style.opacity='0.8';
+}
+
+function present(tweet)
+{
+	if(tweet.getAttribute('inprogress') == 'false')
+	{
+		return;
+	}
+	
+	tweet.style.cursor='';
+	tweet.style.backgroundImage='';
+	tweet.style.opacity='1.0';
+}
+
+function planting(seed, tweet)
+{
+	if(tweet.getAttribute('inprogress') == 'false')
+	{
+		return;
+	}
+	seed.stopPropagation();
 	
 	if(tweet.getAttribute('gardened') == 'true')
 	{
@@ -113,12 +132,12 @@ function gardening(e,tweet)
 	}
 	let children = tweet.children;
 	
-	for(let i=0,cl=children.length;i<cl;i++)
+	for(let i=0, children_len=children.length; i<children_len; i++)
 	{
 		if(children[i].className == 'content')
 		{
 			let x=children[i].children;
-			for(let j=0,ccl=x.length;j<ccl;j++)
+			for(let j=0, x_len=x.length; j<x_len; j++)
 			{
 				if(x[j].className == 'js-tweet-text-container')
 					x[j].innerText=POSITIVE_MESSAGES[Math.floor(Math.random()*POSITIVE_MESSAGES.length)];
